1811297d40a765f672bdc21bf498154a71187781
Juan Manuel Pacheco <juan.manuel.pacheco.arias@gmail.com>
2022-09-08 16:57:06 -0500

    Debugging of the central bug inside the mass unbounb profile

--- Python/Menc_Munb.py
+++ Python/Menc_Munb.py
@@ -11,20 +11,18 @@ folder = sys.argv[1]
 simulation = sys.argv[2]
 name = sys.argv[3]
 
-x, y, z, vx, vy, vz, m, u = np.genfromtxt('/tank0/ballone/coll_set/{}/{}'.format(folder,simulation),
-                                          usecols= (0,1,2,3,4,5,6,8), unpack=True)
+x, y, z, vx, vy, vz, m, d, u = np.genfromtxt('/tank0/ballone/coll_set/{}/{}'.format(folder,simulation),
+                                          usecols= (0,1,2,3,4,5,6,7,8), unpack=True)
 
 # Calculate the position and velocity of the center of mass.
 
-indexm = np.where(m >= np.amax(m)*1e-5)[0]
+xcm = x[np.argmax(d)]
+ycm = y[np.argmax(d)]
+zcm = z[np.argmax(d)]
 
-xcm = np.sum(x[indexm]*m[indexm]) / np.sum(m[indexm])
-ycm = np.sum(y[indexm]*m[indexm]) / np.sum(m[indexm])
-zcm = np.sum(z[indexm]*m[indexm]) / np.sum(m[indexm])
-    
-vcmx = np.sum(vx[indexm]*m[indexm]) / np.sum(m[indexm])
-vcmy = np.sum(vy[indexm]*m[indexm]) / np.sum(m[indexm])
-vcmz = np.sum(vz[indexm]*m[indexm]) / np.sum(m[indexm])
+vcmx = vx[np.argmax(d)]
+vcmy = vy[np.argmax(d)]
+vcmz = vz[np.argmax(d)]
 
 # Calculate the radius and velocity wrt the cm.
 
@@ -36,14 +34,6 @@ v = np.sqrt((vx-vcmx)**2 + (vy-vcmy)**2 + (vz-vcmz)**2)
 index = np.argsort(r)
 
 # Reordering of all quantities based on Radius.
-
-X = x[index]
-Y = y[index]
-Z = z[index]
-
-VX = vx[index]
-VY = vy[index]
-VZ = vz[index]
 
 R = r[index]
 V = v[index]
@@ -61,29 +51,34 @@ G = ((cons.G)/((un.R_sun.to(un.m)**3))*(un.M_sun.to(un.kg))*((1.8845e-2*86400)**
 
 # Energy calculation for the unbound mass criteria.
 
-E = V**2 + U - G*Mencl/R
+E = V**2 - G*Mencl/R
 unb = np.where(E > 0)[0]
+bun = np.where(E <= 0)[0]
 
-Munb = M[unb]
-percent = np.sum(Munb)*100/np.sum(M)
+percent = np.sum(M[unb])*100/np.sum(M)
 
-Mencl_un = np.cumsum(Munb)
+Mencl_un = np.cumsum(M[unb])
 R_un = R[unb]
+Mencl_bn = np.cumsum(M[bun])
+R_bn = R[bun]
+
+# Generation of the profiles.
+
+np.savetxt('/tank0/ballone/coll_set/{}/'.format(folder)+name+'_R.txt', R)
+np.savetxt('/tank0/ballone/coll_set/{}/'.format(folder)+name+'_Mencl.txt', Mencl)
+np.savetxt('/tank0/ballone/coll_set/{}/'.format(folder)+name+'_R_un.txt', R_un)
+np.savetxt('/tank0/ballone/coll_set/{}/'.format(folder)+name+'_Mencl_un.txt', Mencl_un)
+np.savetxt('/tank0/ballone/coll_set/{}/'.format(folder)+name+'_R_bn.txt', R_bn)
+np.savetxt('/tank0/ballone/coll_set/{}/'.format(folder)+name+'_Mencl_bn.txt', Mencl_bn)
 
 with open('/tank0/ballone/coll_set/{}/Munb_percent.txt'.format(folder), 'a') as myfile:
     myfile.write(str(round(percent,4))+'\t'+name+'\n')
 
-# Mass enclosed profile.
-
-Mass_array = np.array([Mencl, R,Mencl_un, R_un])
-
-np.savetxt('/tank0/ballone/coll_set/{}/'.format(folder)+name+'.txt',Mass_array
-           ,delimiter='\t')
-
 fig = plt.figure(figsize=(10, 7))
 
 plt.plot(R,Mencl,'.k', label='Total mass')
-plt.plot(R_un,Mencl_un,'.b', label='Unbound mass')
+plt.plot(R_un,Mencl_un,'.r', label='Unbound mass')
+plt.plot(R_bn,Mencl_bn,'.b', label='Bound mass')
 plt.xlabel("$R$ [$R_{\odot}$]")
 plt.ylabel("$M_{encl}$ [$M_{\odot}$]")
 plt.semilogx()
